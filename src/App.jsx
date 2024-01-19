68daafa86288e4a609fcdd4e19e32fed7cbe8580
WalterGuerra <wsteven00@gmail.com>
2024-01-19 14:17:27 -0500

    add hobbies page with link as well and add hobbies form to hobbies page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Profiles from './pages/Profiles/Profiles'
 import ChangePassword from './pages/ChangePassword/ChangePassword'
 import Calender from './pages/Calender/Calender'
 import Tasks from './pages/Tasks/Tasks'
+import Hobbies from './pages/Hobbies/Hobbies'
 
 // components
 import SideBar from './components/SideBar/SideBar'
@@ -96,6 +97,14 @@ function App() {
           element={
             <ProtectedRoute user={user}>
               <Tasks user={user} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hobbies"
+          element={
+            <ProtectedRoute user={user}>
+              <Hobbies user={user} />
             </ProtectedRoute>
           }
         />
---
+++ src/pages/Hobbies/Hobbies.jsx
@@ -0,0 +1,9 @@
+const Hobbies = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hobbies
