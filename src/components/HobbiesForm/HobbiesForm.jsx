31d9b19bacdbdb8d34f08dc958eb101adc7fcbe7
WalterGuerra <wsteven00@gmail.com>
2024-01-21 14:20:20 -0500

    add formdata state and handler functions to hobby form to handle submit and change

--- src/components/HobbiesForm/HobbiesForm.jsx
+++ src/components/HobbiesForm/HobbiesForm.jsx
@@ -1,8 +1,32 @@
+// npm services 
+import { useState } from "react"
+
 const HobbiesForm = () => {
+  const [formData,setFormData] = useState({
+    title:'',
+    type:'',
+    weeklyGoal: 1
+  })
+
+
+  const handleChange = (evt) => {
+    setFormData({...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setFormData({
+      title: '',
+      type: '',
+      weeklyGoal: 1
+    })
+  }
+
+
   return (
     <div className="max-w-md mx-auto p-4 bg-white rounded-md shadow-md">
       <h2 className="text-2xl font-semibold mb-4">Add A Hobby</h2>
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-gray-600 text-sm font-semibold mb-2">
             Hobby Title:
@@ -11,6 +35,9 @@ const HobbiesForm = () => {
             className="w-full px-3 py-2 border border-gray-300 rounded-md"
             type="text"
             name="title"
+            value={formData.title}
+            required
+            onChange={handleChange}
           />
         </div>
         <div className="mb-4">
@@ -20,6 +47,9 @@ const HobbiesForm = () => {
           <select
             className="w-full px-3 py-2 border border-gray-300 rounded-md"
             name="type"
+            value={formData.type}
+            required
+            onChange={handleChange}
           >
             <option value="">select an option</option>
             <option value="work">Work</option>
@@ -36,6 +66,9 @@ const HobbiesForm = () => {
             type="number"
             name="weeklyGoal"
             min={1}
+            value={formData.weeklyGoal}
+            required
+            onChange={handleChange}
           />
         </div>
         <button
