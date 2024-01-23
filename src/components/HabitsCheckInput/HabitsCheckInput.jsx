9d88ec6f7a2e94c2d7d5958049aa6310879ec004
WalterGuerra <wsteven00@gmail.com>
2024-01-22 20:05:04 -0500

    edit checkbox label for habit form

--- src/components/HabitsCheckInput/HabitsCheckInput.jsx
+++ src/components/HabitsCheckInput/HabitsCheckInput.jsx
@@ -20,16 +20,16 @@ const HabitsCheckInput = (props) => {
 
   return (
     <td key={props.day} className="py-2 px-1 border border-gray-300 text-center">
-      <div className={`checkbox-wrapper-10 ${isChecked ? 'tgl-flip' : ''}`}>
+      <div className={`checkbox-wrapper-10 w-full text-center flex items-center justify-center${isChecked ? 'tgl-flip' : ''}`}>
         <input
-          className="tgl tgl-flip m-auto"
+          className="tgl tgl-flip m-auto text-center"
           id={checkboxId}
           type="checkbox"
           checked={isChecked}
           onChange={handleCheckChange}
         />
         <label
-          className="tgl-btn"
+          className="tgl-btn text-center m-auto"
           data-tg-off="Nope"
           data-tg-on="Yeah!"
           htmlFor={checkboxId}
