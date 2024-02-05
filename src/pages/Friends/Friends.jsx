17e597cb701c0eaf25400d8fef9979d3ebc1afb4
WalterGuerra <wsteven00@gmail.com>
2024-02-05 14:04:46 -0500

    set up friends form on friends page

--- src/pages/Friends/Friends.jsx
+++ src/pages/Friends/Friends.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from "react"
+
 const Friends = () => {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [foundUsers, setFoundUsers] = useState([])
+
+  const handleSearch = async () => {
+
+  }
+
+  const handleSendFriendRequest = (userId) => {
+
+  }
+
   return (
     <div>
-      
+      <form>
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="button" onClick={handleSearch}>
+          Search
+        </button>
+      </form>
+
+      <div>
+        {foundUsers.map((user) => (
+          <div key={user.id}>
+            <span>{user.name}</span>
+            <button onClick={() => handleSendFriendRequest(user.id)}>
+              Add Friend
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
