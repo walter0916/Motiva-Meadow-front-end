26d1aa20d15a3bcc4f52253c20bea64a279b3e47
WalterGuerra <wsteven00@gmail.com>
2024-02-11 15:26:50 -0500

    add service function to get useres messages

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -3,6 +3,17 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/messages`
 
+async function getUserMessages(profileId) {
+  try {
+    const res = await fetch(`${BASE_URL}/${profileId}`, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return await res.json()
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 async function createMessage(senderId, recipientId, messageFormData) {
   try {
     const res = await fetch(`${BASE_URL}/${senderId}/${recipientId}/new`, {
@@ -20,5 +31,6 @@ async function createMessage(senderId, recipientId, messageFormData) {
 }
 
 export {
+  getUserMessages,
   createMessage
 }
