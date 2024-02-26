464669a059d632bd78aee93bf556158e35f582b0
WalterGuerra <wsteven00@gmail.com>
2024-02-26 11:51:36 -0500

    render data for to do lists on to do list card as well as incorporate functionality to see amount of tasks completed out of total tasklength

--- src/components/ToDoListCard/ToDoListCard.jsx
+++ src/components/ToDoListCard/ToDoListCard.jsx
@@ -1,9 +1,19 @@
-const ToDoListCard = () => {
+const ToDoListCard = (props) => {
   return (
-    <div>
-      hi
+    <div className="bg-white rounded-lg shadow-lg overflow-y-auto h-80 p-4">
+      <ul className="space-y-2">
+        {props.usersToDoLists.map((list) => (
+          <li key={list._id} className="text-black rounded-md shadow-md mb-2 px-4 py-2 bg-green-400">
+            <span className="font-bold">{list.title}</span> - {countCompletedTasks(list.tasks)} / {list.tasks.length}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+const countCompletedTasks = (tasks) => {
+  return tasks.filter(task => task.completed).length
 }
 
 export default ToDoListCard
