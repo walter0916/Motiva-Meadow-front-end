7dcc4ea2bab1686b7b126eafe5eca2531ccdd894
WalterGuerra <wsteven00@gmail.com>
2024-03-09 21:27:49 -0500

    add typeing animation to instructions

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,19 @@
 // npm services 
 import { Link } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import SplitType from "split-type"
 import { gsap } from "gsap"
+import { TypeAnimation } from "react-type-animation"
 
 const Home = () => {
 
   useEffect(() => {
     const textAnimation = () => {
       const text = new SplitType("#text")
-      const words = document.querySelectorAll('#text .word')
       gsap.from(text.chars, {
         y: 10,
         duration: 0.5,
-        delay: 0.02,
+        delay: 0.05,
         stagger: 0.05,
       })
     }
@@ -25,21 +25,65 @@ const Home = () => {
       <h1 id='text' className="text-6xl font-bold text-center text-black mb-4" >
         Welcome to Motiva Meadow
       </h1>
-      <p className="text-lg text-center font-semibold text-black mb-6">
-        Where Growth Blossoms: Cultivate Your Potential at Motiva Meadow
-      </p>
+      <TypeAnimation
+        className="text-lg text-center font-semibold text-black mb-6"
+        sequence={[
+          "Where Growth Blossoms: Cultivate Your Potential at Motiva Meadow",
+          1000,
+        ]}
+      />
 
       <div className="mb-8 border border-green-600 rounded-lg shadow-lg p-4">
         <h2 className="text-2xl font-semibold text-black mb-2">How to Use Motiva Meadow</h2>
           <ul className="list-disc pl-6 text-black animate-fadeIn font-semibold">
-            <li>Change your preferences in settings to update what you want to see on your dashboard</li>
-            <li>Add events to see them on the calendar</li>
-            <li>Add habits, goals, and to-do lists to track your progress</li>
-            <li>Archive to-do lists for better organization</li>
-            <li>Set deadlines for goals to see how many days are left</li>
-            <li>View automatically updated stats like habit streaks and to-do list streaks</li>
-            <li>Add friends to check their stats and send them motivational messages</li>
-            <li>Get inspired with motivational quotes on the dashboard</li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[4000, "Change your preferences in settings to update what you want to see on your dashboard"]}
+            />
+          </li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[9000, "Add events to see them on the calendar"]}
+            />
+          </li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[11500, "Add habits, goals, and to-do lists to track your progress"]}
+            />
+          </li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[15000,"Archive to-do lists for better organization"]}
+            />
+          </li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[18000,"Set deadlines for goals to see how many days are left"]}
+            />
+          </li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[21500, "View automatically updated stats like habit streaks and to-do list streaks"]}
+            />
+          </li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[26500,"Add friends to check their stats and send them motivational messages"]}
+            />
+          </li>
+          <li>
+            <TypeAnimation
+              className="mb-2"
+              sequence={[31000,"Get inspired with motivational quotes on the dashboard"]}
+            />
+          </li>
           </ul>
       </div>
 
